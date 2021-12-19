b4009c14dadcabda7e4820cf4b1c4b238b0d28d1
Michael Lebedev <m.l-v@mail.ru>
2021-12-19 20:35:43 +0300

    1

---
+++ script/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $(".lang-btn").on("click", e => {
+        const elem = e.currentTarget;
+        $(".lang-btn").removeClass("active-btn");
+        $(elem).addClass("active-btn");
+    });
+
+// Reg:
+    $("#contact-date").datepicker({
+        maxViewMode: 2,
+        language: "ru",
+        autoclose: true
+    });
+    $(".input-validation-error").parent().css({
+        "--bs-text-opacity": "1",
+        "color" : "rgba(var(--bs-danger-rgb),var(--bs-text-opacity))"
+    });
+});
