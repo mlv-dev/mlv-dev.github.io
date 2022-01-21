972ad2a816de9422394df6ee8d5097619e4ffe99
Michael Lebedev <m.l-v@mail.ru>
2022-01-21 13:55:07 +0300

    p5

--- script/fillflow.js
+++ script/fillflow.js
@@ -1,16 +1,21 @@
-// let path = window.location.pathname;
-// let page = path.split("/").pop();
-// console.log( page );
-
-$('.fillflow-navbar li a').each(function() {
-    if ($(this).attr('href') === location.pathname.split("/").pop()){
-        $(this).addClass('current-page');
-        let color = $(this).css('background-color').slice(0, -1) + ', 0.5)';
-        $(this).css("box-shadow", "0 0 0 0.25rem " + color)
-        // $(this).focus();
-        // $(this).on('blur', () => {
-        //     $(this).focus();
-        // })
-    }
+$(document).ready(function(){
+    $('.fillflow-navbar li a').each(function() {
+        if ($(this).attr('href') === location.pathname.split("/").pop()){
+            $(this).addClass('current-page');
+            let color = $(this).css('background-color').slice(0, -1) + ', 0.5)';
+            $(this).css("box-shadow", "0 0 0 0.25rem " + color);
+        }
+        $(this).on('focus', () => {
+            $(this).css('z-index', 1);
+        });
+        $(this).on('blur', () => {
+            $(this).css('z-index', 2);
+        });
+    });
+    $('.dropdown-toggle').on('click', (event) => {
+        debugger;
+        $(event.currentTarget).dropdown('toggle');
+    }).on('blur', (event) => {
+        $(event.currentTarget).dropdown('toggle');
+    });
 });
-
