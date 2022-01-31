4ae40995152a0935d85d4031b6b74d9ddb7f3530
Michael Lebedev <m.l-v@mail.ru>
2022-02-01 02:52:30 +0300

    p

--- script/fillflow2.js
+++ script/fillflow2.js
@@ -8,4 +8,13 @@ $(document).ready(function(){
     const path = location.pathname.split("/").pop();
     const el = $(`.side-navbar a[href='${path}']`).parent().parent().parent();
     $(el).show();
+
+    $('.side-navbar a').each(function() {
+        if ($(this).attr('href') === location.pathname.split("/").pop()){
+            $(this).addClass('current-page');
+            let color = $(this).css('background-color').slice(0, -1) + ', 0.5)';
+            // $(this).parent().css("box-shadow", "0 0 0 0.25rem " + color);
+            $(this).parent().css("box-shadow", "0 0 12px 0.4rem " + color);
+        }
+    });
 });
