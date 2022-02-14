901bdbe2e5e77b0b8df16309a6bb0e4aa214a17b
Michael Lebedev <m.l-v@mail.rru>
2022-02-14 13:21:13 +0300

    p

--- script/fillflow-content.js
+++ script/fillflow-content.js
@@ -1,14 +1,21 @@
-
-
-$(document).ready(function(){
+$(document).ready(function () {
     function langAct(event) {
         let value = $('#Country').val();
+        const delay = 500;
+
         if (value === 'Россия') {
-            $('.pay-block').show(500);
+            $('.pay-block').show(delay);
+            $('.swift-block').hide(delay);
         } else {
-            $('.pay-block').hide(500);
+            $('.pay-block').hide(delay);
+            $('.swift-block').show(delay);
+        }
+        if (value === 'Выберите страну . . .') {
+            $('.pay-block').hide();
+            $('.swift-block').hide();
         }
     }
+
+    langAct();
     $('#Country').on('change', langAct);
 });
-
